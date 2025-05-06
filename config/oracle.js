4f84f34468308aac41a2ccbbf466cb804794be11
adnageeTT <adnagee@gmail.com>
2025-05-06 19:02:28 -0400

    adding code to repo

---
+++ config/oracle.js
@@ -0,0 +1,32 @@
+import pkg from 'oracledb';
+const { initOracleClient, getConnection } = pkg;
+// import { initOracleClient, getConnection } from 'oracledb';
+import 'dotenv/config';
+
+
+initOracleClient({ libDir: process.env.ORACLE_CLIENT_PATH });
+
+async function insertUser(user) {
+  const connection = await getConnection({
+    user: process.env.ORACLE_USER,
+    password: process.env.ORACLE_PASSWORD,
+    connectString: process.env.ORACLE_CONNECT_STRING
+  });
+
+  await connection.execute(
+    `INSERT INTO users (user_id, first_name, last_name, email, role)
+     VALUES (:id, :fname, :lname, :email, :role)`,
+    {
+      id: user.user_id,
+      fname: user.first_name,
+      lname: user.last_name,
+      email: user.email,
+      role: user.role
+    },
+    { autoCommit: true }
+  );
+
+  await connection.close();
+}
+
+export default insertUser;
